dacbc78b49a358dad1d6ca2e823f2868945a3b96
Zachary Horine <horine.zachary@gmail.com>
2018-11-29 21:27:23 -0800

    add firstFit Decreasing

--- binpack.cpp
+++ binpack.cpp
@@ -105,4 +105,27 @@ int firstFit(int binCapacity, vector<int> caseItems){
   }
   return bins.size();
 }
+
+int firstFitDecreasing(int binCapacity, vector<int> caseItems){
+  int i = 1;
+  while (i < caseItems.size()){  //step through array and sort each value
+    int j = i;
+    while (j > 0 && caseItems.at(j-1) > caseItems.at(j)){ //walk through array with one value and move it down into place
+      int temp = caseItems.at(j-1);
+      caseItems.at(j-1) = caseItems.at(j);
+      caseItems.at(j) = temp;
+      j--;
+    }
+    i++;
+  }
+  for (int i = 0; i < caseItems.size(); i++){
+    cout << caseItems.at(i) << " ";
+  }
+  cout << endl;
+  return firstFit(binCapacity, caseItems);
+}
+
+int bestFit(int binCapacity, vector<int> caseItems){
+
+  return 0;
 }
